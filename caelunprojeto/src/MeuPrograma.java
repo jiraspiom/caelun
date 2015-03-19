55871d466c69db4e3edba1f8c513a0d6b541cef2
jiraspiom <jiraspiom@yahoo.com.br>
2015-03-18 22:47:34 -0300

    Retirado a dependendia da classe pessoa

--- caelunprojeto/src/MeuPrograma.java
+++ caelunprojeto/src/MeuPrograma.java
@@ -1,11 +1,13 @@
-import classes.Pessoa;
-
-
 class MeuPrograma {
 
 	public static void main(String[] args){
 		
 		System.out.print("teste");
 				
-	}	
+	}
+	
+	public void teste(){
+		
+	}
+	
 }
